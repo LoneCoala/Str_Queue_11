daaabde96f9716aa5fd4a1e5182b32c22d7397ac
Даниил Тюрин <tjurin.daniil@rambler.ru>
2022-06-05 15:41:31 +0700

    Добавьте файлы проекта.

---
+++ Str_Queue_11/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Str_Queue_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "A+(45-f(X)*(B-C))"; // текст
+            Stack<int> position = new Stack<int> { }; // левая скобка
+            Stack<int> result = new Stack<int> { }; // все скобки
+            int lengthOfMass = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == '(') // записываю левую скобку
+                {
+                    position.Push(i+1);
+                }
+                if (text[i] == ')') // записываю обе скобки
+                {
+                    result.Push(position.Pop());
+                    result.Push(i+1);
+                    lengthOfMass++;
+                }
+            }
+            int[,] mass = new int[lengthOfMass, 2]; // двумерный массив в котором упорядочим результат
+            for (int i = 0; i < lengthOfMass; i++) // заполняю массив
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    mass[i, j] = result.Pop();
+                }
+            }
+            for (int i = 1; i < lengthOfMass; i++) // упорядочивание по возрастанию
+            {
+                if (mass[i,1] < mass[i-1,1] )
+                {
+                    int swap1 = mass[i,1];
+                    int swap2 = mass[i, 0];
+                    mass[i, 1] = mass[i - 1, 1];
+                    mass[i, 0] = mass[i - 1, 0];
+                    mass[i - 1, 1] = swap1;
+                    mass[i - 1, 0] = swap2;
+                }
+            }
+            for (int i = 0; i < lengthOfMass; i++) // вывод результата
+            {
+                for (int j = 1; j >= 0; j--)
+                {
+                    Console.Write(mass[i, j] + " ");
+                }
+            }
+        }
+    }
+}
